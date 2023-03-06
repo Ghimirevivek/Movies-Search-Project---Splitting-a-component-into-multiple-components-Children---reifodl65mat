813ea43328371e23223a3f11557fe4ef4a98e017
Ghimirevivek <108508449+Ghimirevivek@users.noreply.github.com>
2023-03-06 19:34:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,49 @@
-import React, { useState } from "react";
-import "../styles/App.css";
-import { movies } from "../utils/movieList";
+import React, { useState } from 'react'
+import '../styles/App.css'
+import { movies } from '../utils/movieList'
 
 const App = () => {
-    const [inputValue, setInputValue] = useState("");
-    const [results, setResult] = useState(null);
+  const [inputValue, setInputValue] = useState('')
+  const [results, setResult] = useState(null)
 
-    function handleClick(e) {
-        e.preventDefault()
-        for(let index of movies){
-          if(index.title===inputValue){
-              setResult(inputValue)
-          }  
+  function handleClick(e) {
+    e.preventDefault()
+    setInputValue('')
+    setResult(null)
+    for (let index of movies) {
+      if (index.title === inputValue) {
+        setResult([index])
+      }
     }
-    }
+  }
 
-    function handleChange(e) {
-        setInputValue(e.target.value)
-    }
+  function handleChange(e) {
+    setInputValue(e.target.value)
+  }
 
-    return (
-        <div id="main">
-            <form id="form">
-                <input
-                    type="text"
-                    onChange={handleChange}
-                    value={inputValue}
-                    id="movie-input"
-                ></input>
-                <button id="button" onClick={handleClick}>
-                    Search
-                </button>
-            </form>
-            <div id="result">
-                {results &&
-                    results.map((movie) => (
-                        <div className="movie">{movie}</div>
-                    ))}
+  return (
+    <div id="main">
+      <form id="form">
+        <input
+          type="text"
+          onChange={handleChange}
+          value={inputValue}
+          id="movie-input"
+        ></input>
+        <button id="button" onClick={handleClick}>
+          Search
+        </button>
+      </form>
+      <div id="result">
+        {results &&
+          results.map((movie, index) => (
+            <div className="movie" key={index}>
+              {movie.title}
             </div>
-        </div>
-    );
-};
+          ))}
+      </div>
+    </div>
+  )
+}
 
-export default App;
+export default App
